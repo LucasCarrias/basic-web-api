def1a8ae80290c57250bd851cdcbd303d356ac42
Lucas Carrias <lucascarrias@outlook.com>
2020-12-15 23:39:54 -0300

    Commit inicial

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from profiles.models import Profile
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=127)
+    body = models.TextField()
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
